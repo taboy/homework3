0e751958716d2509852865b51d1e7b25704f6248
Taleh <t.mirzeyev@gmail.com>
2022-02-20 12:20:18 +0300

    finish

--- 24/Program.cs
+++ 24/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("write a number");
+﻿//Задача 24: Найти кубы чисел от 1 до N
+
+
+Console.WriteLine("write a number");
 int num = int.Parse(Console.ReadLine());
 void cube(int num1){
     for(int i=0;i<num1;i++){
