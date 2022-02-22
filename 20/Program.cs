07f5a92bdfb073489f99668b767386f3aa2c9907
a a <t.mirzeyev@mail.com>
2022-02-22 09:50:19 +0300

    finish

---
+++ 20/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("print x: "); 
+ double x = Convert.ToDouble(Console.ReadLine()); 
+ 
+ Console.WriteLine("print y: "); 
+ double y = Convert.ToDouble(Console.ReadLine()); 
+ 
+ if ((x > 0) && (y > 0)) 
+    Console.WriteLine("First quarter "); 
+ else if ((x < 0) && (y > 0)) 
+    Console.WriteLine(" Second Quarter "); 
+ else if((x < 0) && (y < 0)) 
+    Console.WriteLine("Third Quarter "); 
+ else if ((x > 0) && (y < 0)) 
+    Console.WriteLine("Fourth quarter "); 
+else 
+    Console.WriteLine("The point lies on the axis ");
